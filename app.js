bc0c9295ac0e87db31d8eb4101bb5b27860e3016
mukul1169 <mchaudhary.cse17@chitkarauniversity.edu.in>
2020-05-31 11:39:02 +0530

    Phone Book Web App Commit

---
+++ app.js
@@ -0,0 +1,89 @@
+const path = require('path');
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const app = express();
+
+
+const connection=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'PhoneBook'
+});
+
+connection.connect(function(error){
+    if(!!error) console.log(error);
+    else console.log('Database Connected!');
+});
+
+
+//set views file
+app.set('views',path.join(__dirname,'views'));
+
+//set view engine
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.get('/',(req, res) => {
+      let sql = "SELECT * FROM users";
+      let query = connection.query(sql, (err, rows) => {
+       if(err) throw err;
+       // res.send('PHONE BOOK WEB APP');
+        res.render('index', {title : 'PHONE BOOK WEB APP', users : rows});
+    });
+ });
+
+app.get('/add',(req, res) => {
+    res.render('add', {
+        title : 'PHONE BOOK WEB APP'});
+});
+
+
+app.post('/save',(req, res) => {
+    let data = {name: req.body.name, email: req.body.email, phone_no: req.body.phone_no};
+    let sql = "INSERT INTO users SET ?";
+    let query = connection.query(sql, data,(err, results) => {
+      if(err) throw err;
+      res.redirect('/');
+    });
+});
+
+app.get('/edit/:userId',(req, res) => {
+    const userId = req.params.userId;
+    let sql = `Select * from users where id = ${userId}`;
+    let query = connection.query(sql,(err, result) => {
+        if(err) throw err;
+        res.render('edit', {
+            title : 'PHONE BOOK WEB APP', user : result[0] });
+    });
+});
+
+app.post('/update',(req, res) => {
+    const userId = req.body.id;
+    let sql = "update users SET name='"+req.body.name+"',  email='"+req.body.email+"',  phone_no='"+req.body.phone_no+"' where id ="+userId;
+    let query = connection.query(sql,(err, results) => {
+      if(err) throw err;
+      res.redirect('/');
+    });
+});
+
+
+app.get('/delete/:userId',(req, res) => {
+    const userId = req.params.userId;
+    let sql = `DELETE from users where id = ${userId}`;
+    let query = connection.query(sql,(err, result) => {
+        if(err) throw err;
+        res.redirect('/');
+    });
+});
+
+
+// Server Listening
+app.listen(3000, () => {
+console.log('Server is running at port 3000');
+});
+
